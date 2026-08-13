c93e3bc022a577b07b56ff1e475bee125c94f4b7
Moff0061 <malevolentmoff@gmail.com>
2026-08-13 09:56:04 +0000

    madecounter go up

--- tutorials/t02/src/components/PlayerProfile.jsx
+++ tutorials/t02/src/components/PlayerProfile.jsx
@@ -1,11 +1,26 @@
+
+import { useState } from 'react';
+
 function PlayerProfile() {
-    const player = { username: "PixelPioneer", level: 5, active: true };
+    ///const player = { username: "PixelPioneer", level: 5, active: true };
+    const [player, setPlayer] = useState({
+    username: "PixelPioneer",
+    level: 5,
+    active: true
+});
     const { username, level, active } = player;
 
+    const handleLevelUp = () => {
+        const updatedPlayer = { ...player, level: level + 1 };
+        console.log("Updated player state:", updatedPlayer);
+        setPlayer(updatedPlayer);
+    };
+    
     return (
     <div className="profile-card">
         <h2>Player: {username}</h2>
         <p>Current Level: {level}</p>
+        <button onClick={handleLevelUp}>Gain XP</button>
     </div>
 );
 }
