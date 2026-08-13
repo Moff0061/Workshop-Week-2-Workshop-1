d50618a88227c77dab71fe1386dd91d9af6e49ad
Moff0061 <malevolentmoff@gmail.com>
2026-08-13 09:38:16 +0000

    Replaced app contents

--- tutorials/t02/src/App.jsx
+++ tutorials/t02/src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './styles.css'
 import PlayerProfile from './components/PlayerProfile.jsx'
+import './App.css'
 
 function App() {
+
+  return (
     <>
-
-    <PlayerProfile />
-
+      <PlayerProfile />
     </>
+  )
 }
 
-export default App
+export default App;
