ff833bbc51a624cb6391e6b95f09a60aacf98f6a
Moff0061 <malevolentmoff@gmail.com>
2026-08-16 14:10:20 +0000

    Finished up to task 4 module3

--- tutorials/t03/src/App.jsx
+++ tutorials/t03/src/App.jsx
@@ -1,11 +1,35 @@
 import Header from './components/Header.jsx'
+import CourseModule from './components/CourseModule.jsx'
 
 function App() {
+
+  const moduleData = [
+  { 
+    id: 1, 
+    title: "Introduction to React & Vite", 
+    description: "Scaffolding modern environments replacing older legacy bundlers." 
+  },
+  { 
+    id: 2, 
+    title: "JSX Syntax & Rules", 
+    description: "Writing HTML-like markup directly inside our JavaScript files." 
+  },
+  { 
+    id: 3, 
+    title: "Components & Props", 
+    description: "Passing data downwards to build reusable UI elements." 
+  }];
+
+
 
   return (
     <>
     <Header/>
-      <div></div>
+    <div>
+    {moduleData.map((module) => (
+        <CourseModule key={module.id} title={module.title} description={module.description} />
+    ))}
+    </div>
     </>
   )
 }
---
+++ tutorials/t03/src/components/CourseModule.jsx
@@ -0,0 +1,12 @@
+function CourseModule({ title, description }) { 
+    return (
+        <div className="module-card">
+            <h2>{title}</h2>
+            <p>{description}</p>
+        </div>
+    );
+    }
+
+
+    export default CourseModule;
+
